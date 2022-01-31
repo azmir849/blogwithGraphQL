03ec9f25511121380d5312de1d32f3e13afb3a82
azmir849 <azmir15-849@diu.edu.bd>
2022-01-31 16:50:05 +0600

    background img change and footer added

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="mt-5">
+      <p className="text-center mt-5 mb-3"><span className="text-muted text-1xl text-white"> © Copyright {new Date().getUTCFullYear()} Dev Design Biz. All rights reserved.</span></p>
+    </footer>
+  );
+}
