43a4718c4c2e4db606a0ba9e512b1bfa03188896
azmir849 <azmir15-849@diu.edu.bd>
2022-01-31 15:37:36 +0600

    Header categories and logo position update

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,9 +15,9 @@ const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block">
+        <div className="md:float-left block mt-3">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">Graph CMS</span>
+            <span className="cursor-pointer font-bold text-4xl text-white">Dev Design Biz</span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
